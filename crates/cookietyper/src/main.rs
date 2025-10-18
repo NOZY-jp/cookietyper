6880d82d15b7a41d7bc2f0506e117ebeacaf998b
nozy-jp <nozyjp.1214@gmail.com>
2025-10-18 23:54:48 +0900

    implement: cookietyper I/O base using flume

--- crates/cookietyper/src/main.rs
+++ crates/cookietyper/src/main.rs
@@ -1,4 +1,26 @@
+use std::{io::stdin, thread};
+
 use cookietyper_core::Game;
 fn main() {
     let mut game = Game::default();
+
+    let (tx, rx) = flume::bounded(5);
+
+    thread::spawn(move || {
+        loop {
+            let mut s = String::new();
+
+            if let Err(e) = stdin().read_line(&mut s) {
+                eprintln!("{e}");
+            }
+
+            let _ = tx.send(s);
+        }
+    });
+
+    loop {
+        if let Ok(s) = rx.try_recv() {
+            println!("{s}")
+        }
+    }
 }
